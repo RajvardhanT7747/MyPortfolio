98ef91e538c7dc8d3fee4c84ccd631db3af8f6c0
Rajvardhan <rajvardhantipugade5@gmail.com>
2025-10-02 16:26:06 +0530

    Fix ESLint error: Remove unused React import from ScrollColorTransition

--- src/components/ScrollColorTransition.jsx
+++ src/components/ScrollColorTransition.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ScrollColorTransition = () => {
   const [activeSection, setActiveSection] = useState('home');
